6df6c499d05b8b293a3b32f51e1d7a3db5a96818
nikox94 <nikola.yuroukov@gmail.com>
2014-10-31 08:43:07 +0200

    my first com

---
+++ 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+
+    {
+    bool loveprogramming, loveludogorets, programshoudhavenocomments;
+
+    loveprogramming = true;
+    loveludogorets = false;
+    programshoudhavenocomments = true;
+
+    cout<< loveprogramming << endl;
+    cout<< loveludogorets << endl;
+    cout << programshoudhavenocomments << endl;
+    return 0;
+    }
+
