13cea406b150dada700b9e8c4406166b78beaa01
nikox94 <nikola.yuroukov@gmail.com>
2014-11-14 09:16:29 +0200

    blah\

---
+++ Untitled1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    string a[10];
+for (int i=0; i<10; i++)
+ {
+	 getline (cin,a[i]);
+ }
+for (int i=0; i<10; i++)
+{
+	cout<< a[i] << endl;
+
+}
+
+	/* string classmate1 , classmate2 , classmate3 , classmate4 , classmate5 , classmate6 ,classmate7 , classmate8 , classmate9 , classmate10 ,
+		classmate11 , classmate12 , classmate13 , classmate14 , classmate15 , classmate16 , classmate17 , classmate18 , classmate19 ,
+		classmate20 ;
+	cout << "Name1 " ;
+	getline (cin, classmate1);
+	cout <<classmate1<< endl;
+
+	cout << "Name2 " ;
+	getline (cin, classmate2);
+	cout <<classmate2<< endl;
+
+	cout << "Name3 " ;
+	getline (cin, classmate3);
+	cout <<classmate3<< endl;
+
+	cout << "Name4 " ;
+	getline (cin, classmate4);
+	cout <<classmate4<< endl;
+
+	cout << "Name5 " ;
+	getline (cin, classmate5);
+	cout <<classmate5<< endl;
+
+	cout << "Name6 " ;
+	getline (cin, classmate6);
+	cout <<classmate6<< endl;
+
+	cout << "Name7 " ;
+	getline (cin, classmate7);
+	cout <<classmate7<< endl;
+
+	cout << "Name8 " ;
+	getline (cin, classmate8);
+	cout <<classmate8<< endl;
+
+	cout << "Name9 " ;
+	getline (cin, classmate9);
+	cout <<classmate9<< endl;
+
+	cout << "Name10 " ;
+	getline (cin, classmate10);
+
+	cout <<classmate10<< endl;
+	*/
+
+}
+
